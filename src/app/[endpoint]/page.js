0c487d7792cf5dda7a9117b0997f3570dfba16b1
Rahul Sheregar <rahul@Rahuls-MacBook-Air.local>
2024-02-11 12:33:35 -0600

    test it

--- src/app/[endpoint]/page.js
+++ src/app/[endpoint]/page.js
@@ -1,5 +1,5 @@
 import jsondata from "../proposal.json"
-
+export const dynamic = 'force-static';
 export default function EndpointPage({ params }) {
   return <p>Post: {jsondata[params.endpoint]}</p>
 }
