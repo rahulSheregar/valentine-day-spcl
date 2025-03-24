0d249929e015f90e30259c63556e887c89e8ca16
rahulSheregar <156388628+rahulSheregar@users.noreply.github.com>
2025-03-24 14:21:06 -0500

    Update background.js

--- src/app/component/background.js
+++ src/app/component/background.js
@@ -15,20 +15,12 @@ const BackgroundContainer = ({ children, sourceName }) => {
       }}
     >
       <iframe 
-        style={{
-          width: '100%', 
-          maxWidth: '450px', 
-          aspectRatio: '360/640', 
-          height: 'auto', 
-          display: 'block', 
-          margin: '0 auto', 
-          border: 'none',
-        }}
-        src="https://cdn.my7hos.com/content/9f04fcfb-2bdb-4422-83d7-6062a180b17b/posts/c734520a-59e9-421a-a15e-fef9475b5e89/d2f1518e-56d9-428d-be09-913368614b32.mp4" 
-        title="Video Player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-        allowFullScreen
-      />
+    style="width: 100%; max-width: 450px; aspect-ratio: 360/638; height: auto; display: block; margin: 0 auto; border: none; padding-bottom: 177.22222222222223%;" 
+    src="https://cdn.my7hos.com/content/9f04fcfb-2bdb-4422-83d7-6062a180b17b/posts/7074f607-7fac-435a-b039-c4c1e99d92b5/74527e9b-eed2-4c12-9d88-ba59cc79df69_77525c35-3a99-46ea-ac7d-7145840cf831.mp4" 
+    title="Video Player"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+    allowfullscreen>
+  </iframe>
       {children}
     </div>
   );
