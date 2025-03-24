4e43a78ef119ec00fa42cc9ca15d5c78d7327379
rahulSheregar <156388628+rahulSheregar@users.noreply.github.com>
2025-03-24 14:19:15 -0500

    Update background.js

--- src/app/component/background.js
+++ src/app/component/background.js
@@ -3,25 +3,33 @@ import React from 'react';
 
 const BackgroundContainer = ({ children, sourceName }) => {
   return (
-    <div className="background-container" style={{backgroundImage: `url('${sourceName}')`, }}>
-  <iframe 
-    style="width: 100%; max-width: 450px; aspect-ratio: 360/640; height: auto; display: block; margin: 0 auto; border: none; padding-bottom: 177.77777777777777%;" 
-    src="https://cdn.my7hos.com/content/9f04fcfb-2bdb-4422-83d7-6062a180b17b/posts/c734520a-59e9-421a-a15e-fef9475b5e89/d2f1518e-56d9-428d-be09-913368614b32.mp4" 
-    title="Video Player"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-    allowfullscreen>
-  </iframe>
+    <div 
+      className="background-container" 
+      style={{
+        position: 'relative',
+        width: '100%',
+        height: '100vh',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundImage: `url('${sourceName}')`,
+      }}
+    >
+      <iframe 
+        style={{
+          width: '100%', 
+          maxWidth: '450px', 
+          aspectRatio: '360/640', 
+          height: 'auto', 
+          display: 'block', 
+          margin: '0 auto', 
+          border: 'none',
+        }}
+        src="https://cdn.my7hos.com/content/9f04fcfb-2bdb-4422-83d7-6062a180b17b/posts/c734520a-59e9-421a-a15e-fef9475b5e89/d2f1518e-56d9-428d-be09-913368614b32.mp4" 
+        title="Video Player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowFullScreen
+      />
       {children}
-      <style jsx>{`
-        .background-container {
-          position: relative;
-          width: 100%;
-          height: 100vh;
-          background-size: cover;
-          background-position: center;
-          
-        }
-      `}</style>
     </div>
   );
 };
