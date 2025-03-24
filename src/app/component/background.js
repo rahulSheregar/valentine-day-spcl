00b136afc52ba3e3e630144906287995a2e540f8
rahulSheregar <156388628+rahulSheregar@users.noreply.github.com>
2025-03-24 14:23:00 -0500

    Update background.js

--- src/app/component/background.js
+++ src/app/component/background.js
@@ -15,7 +15,16 @@ const BackgroundContainer = ({ children, sourceName }) => {
       }}
     >
       <iframe 
-    style="width: 100%; max-width: 450px; aspect-ratio: 360/638; height: auto; display: block; margin: 0 auto; border: none; padding-bottom: 177.22222222222223%;" 
+   style={{
+  width: '100%',
+  maxWidth: '450px',
+  aspectRatio: '360/640',
+  height: 'auto',
+  display: 'block',
+  margin: '0 auto',
+  border: 'none',
+  paddingBottom: '177.77777777777777%'
+}}
     src="https://cdn.my7hos.com/content/9f04fcfb-2bdb-4422-83d7-6062a180b17b/posts/7074f607-7fac-435a-b039-c4c1e99d92b5/74527e9b-eed2-4c12-9d88-ba59cc79df69_77525c35-3a99-46ea-ac7d-7145840cf831.mp4" 
     title="Video Player"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
