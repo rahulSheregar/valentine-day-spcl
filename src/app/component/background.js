d19bc9f42b1dc5e425d02e9871c2998db2c61412
rahulSheregar <156388628+rahulSheregar@users.noreply.github.com>
2025-04-01 12:44:23 -0500

    Update background.js

--- src/app/component/background.js
+++ src/app/component/background.js
@@ -51,7 +51,7 @@ const BackgroundContainer = ({ children, sourceName }) => {
       <div className="video-container">
         <iframe
           id="videoFrame"
-          src="https://localhost:5173/embed-collect-response/9f04fcfb-2bdb-4422-83d7-6062a180b17b/793cc763-82f0-49a6-95d9-a4ebcd70f154/eb641c8d-fdbf-465a-a66a-158813729737?_branch_match_id=1396713678641974204&utm_source=braid-app&utm_medium=participation-link&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXz8nMy9YrSS0u0S3LTEnNLy5IzNNLzs%2FVTy1z8fWpyjExCkqyrytKTUstKsrMS49PKsovL04tsnXOKMrPTQUA7qhMrEUAAAA%3D"
+          src="https://app.test-videospan.com/embed-collect-response/9f04fcfb-2bdb-4422-83d7-6062a180b17b/793cc763-82f0-49a6-95d9-a4ebcd70f154/eb641c8d-fdbf-465a-a66a-158813729737?_branch_match_id=1392874297684744850&utm_source=braid-app&utm_medium=participation-link&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXz8nMy9YrSS0u0S3LTEnNLy5IzNNLzs%2FVTy1z8fWpyjExCkqyrytKTUstKsrMS49PKsovL04tsnXOKMrPTQUA7qhMrEUAAAA%3D"
           title="Video Player"
           frameBorder="0"
           allow="camera; microphone; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
